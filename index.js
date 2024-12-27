2db659c90192825d979baec545f5e6cb64b8a309
Noel Lonka <noel.lonka@gmail.com>
2024-12-27 18:41:45 +0200

    exercise 3.2 done (puhelinluettelon backend step2)

--- index.js
+++ index.js
@@ -34,6 +34,11 @@ app.get("/api/persons", (request, response) => {
 	response.json(persons);
 });
 
+app.get("/api/info", (request, response) => {
+	response.send(`<p>Phonebook has info for ${persons.length} people</p>
+		<p>${new Date(Date.now()).toString()}</p>`);
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
