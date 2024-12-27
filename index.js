bc285c1a53ac69a9bb12a68c384f5724842f54c2
Noel Lonka <noel.lonka@gmail.com>
2024-12-27 19:54:47 +0200

    exercise 3.6 done (puhelinluettelon backend step6)

--- index.js
+++ index.js
@@ -60,6 +60,24 @@ app.delete("/api/persons/:id", (request, response) => {
 app.post("/api/persons", (request, response) => {
 	const body = request.body;
 
+	if (!body.name) {
+		return response.status(400).json({
+			error: "name missing",
+		});
+	}
+
+	if (!body.number) {
+		return response.status(400).json({
+			error: "number missing",
+		});
+	}
+
+	if (persons.find((person) => person.name === body.name)) {
+		return response.status(400).json({
+			error: "name must be unique",
+		});
+	}
+
 	const person = {
 		id: String(Math.floor(Math.random() * 100000)),
 		name: body.name,
